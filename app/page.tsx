9f0e38fb961467ea6f30acdcc45be9087a8aecb5
YOKI FERDINAND SITUMORANG <yoki.situmorang@assarent.co.id>
2024-08-29 11:48:44 +0700

    add rorate word

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ import * as React from "react";
 import { NextUIProvider } from "@nextui-org/react";
 import { Input } from "@nextui-org/input";
 import Ripple from "@/components/magicui/ripple";
-
+import WordRotate from "@/components/magicui/word-rotate";
 export default function Home() {
   return (
 
@@ -20,16 +20,19 @@ export default function Home() {
           {/* <MagicCard className="w-24 h-24"></MagicCard> */}
           <div className="relative flex h-screen w-full flex-col items-center justify-center overflow-hidden rounded-lg bg-background md:shadow-xl ">
             <p className="z-10 whitespace-pre-wrap text-center text-5xl font-medium tracking-tighter text-white">
-            Connected
+              <WordRotate
+                className="text-4xl font-bold text-slate-600 dark:text-white mx-4"
+                words={["We Are Connected", "We Are Awesome", "Stay Tune For Update UpdateUpdate"]}
+              />
             </p>
             <Ripple />
           </div>
-          <div className="relative flex h-screen w-full flex-col items-center justify-center overflow-hidden rounded-lg border bg-background md:shadow-xl">
+          {/* <div className="relative flex h-screen w-full flex-col items-center justify-center overflow-hidden rounded-lg border bg-background md:shadow-xl">
             <p className="z-10 whitespace-pre-wrap text-center text-5xl font-medium tracking-tighter text-white">
               Connected
             </p>
             <Ripple />
-          </div>
+          </div> */}
 
 
 
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -17,21 +17,58 @@ import { link as linkStyles } from "@nextui-org/theme";
 import NextLink from "next/link";
 import clsx from "clsx";
 
+import React from "react";
 
 export const Navbar = () => {
-
-
+    const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+    const menuItems = [
+        "Profile",
+        "Dashboard",
+        "Activity",
+        "Analytics",
+        "System",
+        "Deployments",
+        "My Settings",
+        "Team Settings",
+        "Help & Feedback",
+        "Log Out",
+    ];
     return (
-        <NextUINavbar maxWidth="xl" position="sticky" className=" bg-transparent w-full mt-2" isBlurred={false}>
+        <NextUINavbar onMenuOpenChange={setIsMenuOpen} maxWidth="xl" position="sticky" className=" bg-transparent w-full mt-2" isBlurred={false}>
+            {/* Burger Menu */}
+            <NavbarContent>
+                <NavbarMenuToggle
+                    aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+                    className="lg:hidden"
+                />
+            </NavbarContent>
+            <NavbarMenu>
+                {menuItems.map((item, index) => (
+                    <NavbarMenuItem key={`${item}-${index}`}>
+                        <Link
+                            color={
+                                index === 2 ? "primary" : index === menuItems.length - 1 ? "danger" : "foreground"
+                            }
+                            className="w-full"
+                            href="#"
+                            size="lg"
+                        >
+                            {item}
+                        </Link>
+                    </NavbarMenuItem>
+                ))}
+            </NavbarMenu>
+
+
             <div className="h-full w-full py-2 justify-center flex ">
                 <div className=" border-gray-500 border-opacity-15 backdrop-blur-md rounded-md flex min-w-min items-center px-4">
-                    <Avatar
+                    {/* <Avatar
                         className=" scale-90"
                         color="secondary"
                         name="Jason Hughes"
                         size="sm"
                         src="https://i.pravatar.cc/150?u=a042581f4e29026704d"
-                    />
+                    /> */}
                     <div className="ml-2 flex space-x-2 gap-1">
                         <NavbarContent >
                             <ul className="hidden lg:flex gap-4 justify-start ml-2">
