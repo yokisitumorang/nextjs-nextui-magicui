8efc3b2e5b1070589e7ccca88ae7996ae8732493
YOKI FERDINAND SITUMORANG <yoki.situmorang@assarent.co.id>
2024-08-29 11:12:47 +0700

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -2,25 +2,35 @@
 
 import Image from "next/image";
 import BlurIn from "@/components/magicui/blur-in";
+import { MagicCard } from "@/components/magicui/magic-card";
 import * as React from "react";
-import {NextUIProvider} from "@nextui-org/react";
-import {Input} from "@nextui-org/input";
+import { NextUIProvider } from "@nextui-org/react";
+import { Input } from "@nextui-org/input";
+import Ripple from "@/components/magicui/ripple";
 
 export default function Home() {
   return (
-    
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     
-     <div>
-     <NextUIProvider>
-      {/* <BlurIn word={"Hello World"}></BlurIn> */}
-      <Input type="email" label="Email" placeholder="Enter your email" />
-    </NextUIProvider>
-      
-    </div>
-     
 
-      
+    <main className="">
+
+      <div>
+        <NextUIProvider className="w-full h-full">
+          {/* <BlurIn word={"Hello World"}></BlurIn> */}
+          {/* <Input type="email" label="Email" placeholder="Enter your email" /> */}
+          {/* <MagicCard className="w-24 h-24"></MagicCard> */}
+
+          <div className="relative flex h-[500px] w-full flex-col items-center justify-center overflow-hidden rounded-lg border bg-background md:shadow-xl">
+            <p className="z-10 whitespace-pre-wrap text-center text-5xl font-medium tracking-tighter text-white">
+              Ripple
+            </p>
+            <Ripple />
+          </div>
+        </NextUIProvider>
+
+      </div>
+
+
+
     </main>
   );
 }
